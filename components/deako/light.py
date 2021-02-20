de80d19d663e27a47acb532959fd8d743757a735
William Bartholomew <wdb@willbar.com>
2021-02-19 22:58:27 -0800

    Update light.py

--- components/deako/light.py
+++ components/deako/light.py
@@ -1,71 +1,56 @@
-import json
+import asyncio
+from datetime import timedelta
 import logging
-from telnetlib import Telnet
-import uuid
 
+import async_timeout
 import voluptuous as vol
 
+from .const import DOMAIN
+
 from homeassistant.components.light import LightEntity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    client = DeakoClient("172.17.18.125")
-    devices = client.deviceList()
-    lights = [DeakoLight(client, device) for device in devices]
-    async_add_entities(lights)
+async def async_setup_entry(hass, entry, async_add_entities):
+    client = hass.data[DOMAIN][entry.entry_id]
+
+    async def async_update_data():
+        try:
+            async with async_timeout.timeout(10):
+                return await client.deviceList()
+        except ApiError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="light",
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=5),
+    )
+
+    await coordinator.async_refresh()
+
+    async_add_entities(
+        DeakoLight(coordinator, v, client) for (k, v) in enumerate(coordinator.data)
+    )
 
 
-class DeakoClient:
-    def __init__(self, host):
-        self._host = host
-
-    def control(self, target, power, **kwargs):
-        tn = Telnet(self._host, 23)
-        self._send(
-            tn,
-            "CONTROL",
-            target=target.upper(),
-            state={"power": power, "dim": 100},
-        )
-        response = self._receive(tn)
-        tn.close()
-        return response
-
-    def deviceList(self):
-        tn = Telnet(self._host, 23)
-        self._send(tn, "DEVICE_LIST")
-        response = self._receive(tn)
-        number_of_devices = int(response["data"]["number_of_devices"])
-        devices = []
-        for _ in range(1, number_of_devices):
-            response = self._receive(tn)
-            devices.append(response)
-        tn.close()
-        return devices
-
-    def _send(self, client, command, **kwargs):
-        message = f'{{"transactionId": "{uuid.uuid4()}", "type": "{command}", "dst": "deako", "src": "hass", "data": {json.dumps(kwargs)}}}\r\n'
-        print(message)
-        client.write(
-            bytes(
-                message,
-                "ascii",
-            )
-        )
-
-    def _receive(self, client):
-        response = json.loads(client.read_until(b"\r\n"))
-        return response
+class ApiError(Exception):
+    pass
 
 
-class DeakoLight(LightEntity):
-    def __init__(self, client, response):
+class DeakoLight(CoordinatorEntity, LightEntity):
+    def __init__(self, coordinator, uuid, client):
+        super().__init__(coordinator)
         self._client = client
-        self._uuid = response["data"]["uuid"]
-        self._name = response["data"]["name"]
-        self._state = response["data"]["state"]["power"]
+        self._uuid = uuid
 
     @property
     def unique_id(self):
@@ -73,18 +58,16 @@ class DeakoLight(LightEntity):
 
     @property
     def name(self):
-        """Return the display name of this light."""
-        return self._name
+        return self.coordinator.data[self.unique_id]["name"]
 
     @property
     def is_on(self):
-        """Return true if light is on."""
-        return self._state
+        return self.coordinator.data[self.unique_id]["state"]["power"]
 
-    def turn_on(self, **kwargs):
-        self._client.control(self._uuid, True)
-        self._state = True
+    async def async_turn_on(self, **kwargs):
+        await self._client.control(self._uuid, True)
+        await self.coordinator.async_request_refresh()
 
-    def turn_off(self, **kwargs):
-        self._client.control(self._uuid, False)
-        self._state = False
+    async def async_turn_off(self, **kwargs):
+        await self._client.control(self._uuid, False)
+        await self.coordinator.async_request_refresh()
